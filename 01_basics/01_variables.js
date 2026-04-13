1d88faf67b08243e3224844ade441a52921ec541
nimrahbanu <112688177+nimrahbanu@users.noreply.github.com>
2026-04-13 09:52:55 +0000

    update data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1212
+let accountEmail = "aa@gmail.com"
+var accountName = "aaa"
+let undefinedvalue
+var varundefinedvalue
+  
+  accountEmail = "update@gmail.com"
+  accountName = "update"
+
+console.table([accountId, accountEmail,accountName, undefinedvalue, varundefinedvalue])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,4 @@
+let state; 
+console.log(typeof state); //undefined
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
