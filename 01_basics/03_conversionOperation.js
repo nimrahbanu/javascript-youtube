aa44f4e8d74919ad478f16da9c727894506693f3
nimrahbanu <112688177+nimrahbanu@users.noreply.github.com>
2026-04-14 06:22:37 +0000

    updated

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,70 @@
+// let score = 33
+// console.log(typeof score) // number
+
+// let numbervalue = "12acs"
+// let valueofnumber = Number(numbervalue)
+// console.log(typeof valueofnumber)
+// console.log(valueofnumber); //NaN
+
+// let str1 = "nimrah"
+// let str2 = "banu"
+// console.log(str1 + ' '+str2) //nimrah banu
+
+// console.log('1'+2) //12
+// console.log(1+'1'+2)//112
+// console.log('1'+'1'+'2')//112
+
+// console.log(1+1+'2')//22
+// console.log('1'+'1'+2)//112 
+
+
+// let value = -33
+// let stringnumberconversion = String(value)
+// console.log(typeof stringnumberconversion) //string
+
+// console.log(+true) //1
+// console.log(+"") //0
+
+// let num1, num2, num3 
+// num1 =num2 = num3 = 2+2
+// console.table({num1, num2, num3})
+//  let gamecounter = 100
+// console.log(gamecounter);
+
+//  ++gamecounter
+//  console.log(gamecounter) // 101
+
+// gamecounter++
+// console.log(gamecounter); //101
+
+// let x = 3;
+// let y = x++;
+// console.log(y)//3
+// const z = y++;
+// console.log(`x:${x}, y:${y}, z:${z}`);
+// // x:4, y:4, z:3
+
+// let a = 3;
+// let b = ++a;
+// const c= ++b;
+// console.log(`a:${a}, b:${b}, c:${c}`);
+
+// a:4, b:5, c:5
+
+
+//comparision
+// console.log("2" > 1); //true
+// console.log("02" > 1); //true
+// console.log(null > 0) //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+// console.log(null === 0); //false
+// console.log(null != 0); //true
+
+
+// console.log(`undefined > 0 `, undefined > 0) //false
+// console.log(undefined == 0); //false
+// console.log(undefined >= 0); //false
+// console.log(undefined === 0); //false
+// console.log(undefined != 0); //true
+// console.log(undefined < 0); //false
---
+++ 01_basics/04_jsSummary.js
@@ -0,0 +1,31 @@
+//Primitive -> 7
+// String,
+// Number, 
+//  Boolean,
+// null(1 dm khali empty)
+// undefined, 
+// Symbol
+// BigInt
+
+
+//Non-Primitive(refrence type)
+//Array, Objects,  functions
+
+//JavaScript is a dynamically typed language. 
+//In JavaScript, you don't need to specify the data type of a variable (like "string" or "number") when you declare it. Instead, the data type is determined at runtime based on the value currently assigned to the variable
+
+// console.log(typeof (null)) //object
+// const id = Symbol('123')
+// const second_id = Symbol('123')
+// console.table({id, second_id})
+// console.table(id === second_id) //false
+// console.log(typeof id) //symbol
+
+//  const bigNumber = 6876865756767621n
+//  console.log(typeof bigNumber) //bigint
+
+ 
+function myFInction(){
+    console.log("hello")
+}
+console.log(typeof myFInction); // function
